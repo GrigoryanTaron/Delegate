b92c82861fa8c50bd2b23bd57e4a508c3fb95a82
Taron Grigoryan <1991taron@gmail.com>
2022-06-17 16:01:32 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegate
+{
+    delegate void Operation(string str);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> list = new List<string>();
+            list.Add("jdy");
+            list.Add("sth");
+            list.Add("sytterthht");
+            list.Add("uujmjgghm");
+            list.Add("dyjhdhdas");
+            list.Add("djhhgj");
+           var result= list._Last(x=>x.Contains("j"));
+            Operation op;
+            op = Print;
+            op(result);
+           
+        }
+        public static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ _Last.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public static class Extension
+    {
+        public static T _Last<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (predicate == null) throw new ArgumentNullException("predicate");
+            T res = default(T);
+            bool found = false;
+            foreach (T element in source)
+            {
+                if (predicate(element))
+                {
+                    res = element;
+                    found = true;
+                }
+            }
+            if (found) return res;
+            throw new KeyNotFoundException();
+        }
+    }
+}
